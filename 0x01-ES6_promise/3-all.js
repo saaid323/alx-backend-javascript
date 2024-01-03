45161edc422a14cd3aa5b7d3ddf9cf5d1a9a922c
saaid323 <saaid2538@gmail.com>
2024-01-03 06:10:43 +0000

    added 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,6 @@
-import { uploadPhoto, createUser } from './utils'
+import { uploadPhoto, createUser } from './utils';
+
 export default function handleProfileSignup() {
-	if (true) {
-		Promise.all([uploadPhoto(), createUser()]).then(([upload, create]) => 
-		console.log(upload.body, create.firstName, create.lastName))
-	}
+  Promise.all([uploadPhoto(), createUser()]).then(([upload, create]) => console.log(upload.body,
+    create.firstName, create.lastName));
 }
