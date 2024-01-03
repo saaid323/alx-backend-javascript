16d82da1ac6bb2f1a1ca5141af84aa9379656a2d
saaid323 <saaid2538@gmail.com>
2024-01-03 04:18:15 +0000

    fixed eslint errors

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,11 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   const i = [];
   for (const idx of array) {
-    // let value = array[idx];
-    // array[idx] = appendString + value;
-	  const word = appendString + idx;
-	  i.push(word);
+    const word = appendString + idx;
+    i.push(word);
   }
-
   return i;
 }
--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,10 @@
 export default function createIteratorObject(report) {
   const i = [];
   for (const departments in report.allEmployees) {
-    i.push(...report.allEmployees[departments]);
+    /* guard-for-in */
+    if (Object.prototype.hasOwnProperty.call(report.allEmployees, departments)) {
+      i.push(...report.allEmployees[departments]);
+    }
   }
   return i;
 }
