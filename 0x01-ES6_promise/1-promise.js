ac7f9aecff8b14252ca4a6d713d69fbb2d1a23e2
saaid323 <saaid2538@gmail.com>
2024-01-03 04:46:57 +0000

    added 0-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,8 @@
 export default function getFullResponseFromAPI(success) {
-  return new Promise((reject, resolve) => {
+  return new Promise((resolve, reject) => {
     if (success) {
-      // eslint-disable-next-line prefer-promise-reject-errors
       resolve({ status: 200, body: 'Success' });
     } else {
-      // eslint-disable-next-line prefer-promise-reject-errors
       reject(new Error('The fake API is not working currently'));
     }
   });
