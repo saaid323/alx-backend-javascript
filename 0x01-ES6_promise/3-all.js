e16b3abada61b86432b5decdedaa567a8da89802
saaid323 <saaid2538@gmail.com>
2024-01-03 06:15:32 +0000

    added 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,5 +2,5 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then(([upload, create]) => console.log(upload.body,
-    create.firstName, create.lastName));
+    create.firstName, create.lastName)).catch(() => console.log('Signup system offline'));
 }
