f183ecd7d0269f23de9b7d58861a7e824af91b6e
saaid323 <saaid2538@gmail.com>
2024-01-03 07:23:06 +0000

    added 5-photo-reject.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,10 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  uploadPhoto(fileName);
-  signUpUser(firstName, lastName);
-  return new Promise(() => {
-  });
+  const photo = uploadPhoto(fileName);
+  const user = signUpUser(firstName, lastName);
+  const all = [photo, user];
+  return Promise.allSettled(all).then((results) =>
+   results.forEach((result) =>
+    console.log({status: result.status, value: result.value || result.reason})));
 }
