cdbe6cad0f5749c79917e687e550771b6396e7d2
saaid323 <saaid2538@gmail.com>
2024-01-03 05:08:45 +0000

    added 2-main.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-	return promise.then((response) => {
-		return {'status': 200, 'body': 'success'}
-	})
-		.catch((error) => {
-			return {}, erro
-		})
-	.finally(() => { console.log('Got a response from the API')});
+  return promise.then(() => ({ status: 200, body: 'success' }))
+    .catch(() => { return {} })
+    .finally(() => { console.log('Got a response from the API'); });
 }
