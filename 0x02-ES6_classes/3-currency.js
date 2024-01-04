d2c7e97ff7289d59fe8408aae85d5aecc195e09c
saaid323 <saaid2538@gmail.com>
2024-01-04 10:27:08 +0000

    added 3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,7 +1,7 @@
 export default class Currency {
   constructor(code, name) {
-    this._code = code;
-    this._name = name;
+    this.code = code;
+    this.name = name;
   }
 
   get code() {
@@ -21,6 +21,6 @@ export default class Currency {
   }
 
   displayFullCurrency() {
-    return `${this._name} ${this._code}`;
+    return `${this.name} (${this.code})`;
   }
 }
