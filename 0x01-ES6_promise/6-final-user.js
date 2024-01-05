cca96e1886e9292b269a1c2ba5a71523e87d0ca7
saaid323 <saaid2538@gmail.com>
2024-01-05 15:39:08 +0000

    added 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,17 +4,9 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const photoPromise = uploadPhoto(fileName);
   const userPromise = signUpUser(firstName, lastName);
-
-  return Promise.allSettled([photoPromise, userPromise]).then((results) => results.map((result) => {
-    if (result.status === 'fulfilled') {
-      return {
-        status: 'fulfilled',
-        value: result.value,
-      };
-    }
-    return {
-      status: 'rejected',
-      value: result.reason.message,
-    };
-  }));
+  return Promise.allSettled([photoPromise, userPromise])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : String(result.reason),
+    })));
 }
