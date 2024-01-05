e6651dc0b4163fc563fa3e5348500923f86011cd
saaid323 <saaid2538@gmail.com>
2024-01-05 15:23:06 +0000

    added 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,11 +5,5 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const photo = uploadPhoto(fileName);
   const user = signUpUser(firstName, lastName);
   const all = [photo, user];
-  return Promise.any(all)
-    .then((results) => results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return [{ status: result.status, value: result[1] }];
-      }
-      return [{ status: result.status, value: result.reason.message }];
-    }));
+  return Promise.allSettled(all).then((results) => results);
 }
