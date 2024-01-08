a233f6d228d68bc1fb408c21a07fe487521c881e
saaid323 <saaid2538@gmail.com>
2024-01-08 21:14:22 +0000

    added 10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(map) {
+  if (map instanceof Map) {
+    return 'Cannot process';
+  }
   for (const [key, value] of map) {
     if (value === 1) {
       console.log(value);
