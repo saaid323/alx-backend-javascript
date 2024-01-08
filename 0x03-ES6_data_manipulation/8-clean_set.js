57c0ac5bd2e95fd2c350a784631c0626486e9a0c
saaid323 <saaid2538@gmail.com>
2024-01-08 16:40:46 +0000

    added 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,20 +1,20 @@
-export default function cleanSet(set, str){
-	let arr = [];
-	let text = '';
-	if (str === '') {
-		return '';
-	}
-	for (let i of set) {
-		if (i.startsWith(str)) {
-			arr.push(i.slice(str.length));
-		}
-	}
-	for (let i in arr) {
-		if (i < arr.length -1) {
-			text += arr[i] + '-';
-		} else {
-			text += arr[i];
-		}
-	}
-	return text;
+export default function cleanSet(set, str) {
+  const arr = [];
+  let text = '';
+  if (str === '') {
+    return '';
+  }
+  for (const i of set) {
+    if (i.startsWith(str)) {
+      arr.push(i.slice(str.length));
+    }
+  }
+  for (const i in arr) {
+    if (i < arr.length - 1) {
+      text += `${arr[i]}-`;
+    } else {
+      text += arr[i];
+    }
+  }
+  return text;
 }
