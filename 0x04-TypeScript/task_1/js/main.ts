fd885d259466cd53b131acfee7666bb00d8fdf5a
saaid323 <saaid2538@gmail.com>
2024-01-10 12:56:24 +0000

    added Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -14,3 +14,20 @@ interface Directors extends Teacher {
 function printTeacher(firstName: string, lastName: string) {
     return `${firstName[0]}. ${lastName}`;
 }
+
+class StudentClass {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+    displayName(): string {
+        return this.firstName;
+    }
+
+}
