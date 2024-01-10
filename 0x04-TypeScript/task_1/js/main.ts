d259d29e15ba25451e02941c154f3fcdf87da3ea
saaid323 <saaid2538@gmail.com>
2024-01-10 12:24:10 +0000

    added Extending the Teacher class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,8 +1,12 @@
-interface T {
+interface Teacher {
     readonly firstName: string,
     readonly lastName: string,
     fullTimeEmployee: boolean,
     yearsOfExperience?: number,
     location: string,
     [key: string]: any,
+}
+
+interface Directors extends Teacher {
+    numberOfReports: number,
 }
