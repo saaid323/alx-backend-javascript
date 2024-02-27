7c034cb76e1a2583b1d7888c25b09d37593664c1
saaid323 <saaid2538@gmail.com>
2024-02-27 08:53:45 +0000

    added 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,10 +1,11 @@
 const app = require('http');
 
 const server = app.createServer((req, res) => {
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.write('Hello Holberton School!');
   res.end();
 });
 
-server.listen(1245);
+server.listen(1245, 'localhost', () => {});
 module.exports = app;
