66cbd4dd7efbf85a812e9c45bda516b31bd85021
saaid323 <saaid2538@gmail.com>
2024-02-27 08:45:17 +0000

    added 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,10 @@
-const http = require('http');
+const app = require('http');
 
-const server = http.createServer((req, res) => {
+const server = app.createServer((req, res) => {
   res.setHeader('Content-Type', 'plain/Text');
   res.write('Hello Holberton School!');
   res.end();
 });
 
 server.listen(1245);
+module.exports = app;
