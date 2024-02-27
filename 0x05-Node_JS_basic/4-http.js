68f0046f439700931710f2d5770f4d8865b507b4
saaid323 <saaid2538@gmail.com>
2024-02-27 09:01:42 +0000

    added 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,16 +1,15 @@
-const app = require('http');
+const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const server = app.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  // res.write('Hello Holberton School!');
   res.end('Hello Holberton School!');
 });
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 module.exports = app;
