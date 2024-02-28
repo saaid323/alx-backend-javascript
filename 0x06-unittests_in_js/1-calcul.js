fb7eea96f9636657ec3767fb584f4373eae986de
saaid323 <saaid2538@gmail.com>
2024-02-28 15:21:45 +0000

    added 1-calcul.js, 1-calcul.test.js

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,15 @@
+function calculateNumber (type, a, b) {
+    let result;
+    a = Math.round(a);
+    b = Math.round(b);
+    if (type === 'SUM') {
+      result = a + b;
+    } else if (type === 'SUBTRACT') {
+      result = a - b;
+    } else if (type === 'DIVIDE') {
+      result = a / b;
+    }
+    return result;
+}
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,15 @@
+const calculateNumber = require('./1-calcul');
+const assert = require('assert');
+
+describe('calculateNumber', function() {
+  it('calculator', function() {
+    assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
+    assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    assert.strictEqual(calculateNumber('', 1.4, 4.5), undefined);
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), Infinity);
+    assert.strictEqual(calculateNumber('DIVIDE', '', ''), NaN);
+    assert.strictEqual(calculateNumber('SUM', '', ''), 0);
+    assert.strictEqual(calculateNumber('SUBTRACT', '', ''), 0);
+  });
+});
