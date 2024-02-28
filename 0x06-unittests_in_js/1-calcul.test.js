83e8ca6ac47d2bc76dd43d44844e3fd160971ee2
saaid323 <saaid2538@gmail.com>
2024-02-28 16:44:51 +0000

    added 1-calcul.js, 1-calcul.test.js

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -2,16 +2,46 @@ const calculateNumber = require('./1-calcul');
 const assert = require('assert');
 
 describe('calculateNumber', function() {
-  it('calculator', function() {
-    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
-    assert.strictEqual(calculateNumber('SUM', -1.4, -4.5),-5);
-    assert.strictEqual(calculateNumber('SUM', 0, 0), 0);
-    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+  describe('sum', function() {
+    it('Both positive numbers', function() {
+      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+    it('Both negative numbers', function() {
+      assert.strictEqual(calculateNumber('SUM', -1.4, -4.5),-5);
+    });
+    it('Both zero', function() {
+      assert.strictEqual(calculateNumber('SUM', 0, 0), 0);
+    });
+    it('one negative one positive', function() {
+      assert.strictEqual(calculateNumber('SUM', -1, 2), 1);
+    });
+  });
+  describe('subtract', function() {
+    it('Both positive numbers', function() {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+    it('Both negative numbers', function() {
     assert.strictEqual(calculateNumber('SUBTRACT', -1.4, -4.5), 3);
+    });
+    it('Both zero', function() {
     assert.strictEqual(calculateNumber('SUBTRACT', 0, 0), 0);
-    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-    assert.strictEqual(calculateNumber('DIVIDE', 0, 4.5), 0);
+    });
+    it('one negative one positive', function() {
+      assert.strictEqual(calculateNumber('SUBTRACT', -1, 2), -3);
+    });
+  });
+  describe('divide', function() {
+    it('Both positive numbers', function() {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+    it('one zero one positive', function() {
+      assert.strictEqual(calculateNumber('DIVIDE', 0, 4.5), 0);
+    });
+    it('one zero one negative', function() {
     assert.strictEqual(calculateNumber('DIVIDE', 0, -4.5), -0);
-    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), NaN);
+    });
+    it('zero division', function() {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), NaN);
+    });
   });
 });
