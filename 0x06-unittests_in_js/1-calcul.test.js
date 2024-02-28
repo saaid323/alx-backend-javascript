de0b1a1fbdb28640ae254440500521aad3327004
saaid323 <saaid2538@gmail.com>
2024-02-28 15:51:21 +0000

    added 0-calcul.js, 0-calcul.test.js

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -6,10 +6,6 @@ describe('calculateNumber', function() {
     assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
     assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
     assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-    assert.strictEqual(calculateNumber('', 1.4, 4.5), 0);
     assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), NaN);
-    assert.strictEqual(calculateNumber('DIVIDE', '', ''), NaN);
-    assert.strictEqual(calculateNumber('SUM', '', ''), 0);
-    assert.strictEqual(calculateNumber('SUBTRACT', '', ''), 0);
   });
 });
