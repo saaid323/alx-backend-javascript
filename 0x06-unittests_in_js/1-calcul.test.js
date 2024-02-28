9f9312d8e127832437763d7e588b21d10cbc94e9
saaid323 <saaid2538@gmail.com>
2024-02-28 16:24:00 +0000

    added 1-calcul.js, 1-calcul.test.js

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -3,15 +3,15 @@ const assert = require('assert');
 
 describe('calculateNumber', function() {
   it('calculator', function() {
-    assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
-    assert.equal(calculateNumber('SUM', -1.4, -4.5),-5);
-    assert.equal(calculateNumber('SUM', 0, 0), 0);
-    assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
-    assert.equal(calculateNumber('SUBTRACT', -1.4, -4.5), 3);
-    assert.equal(calculateNumber('SUBTRACT', 0, 0), 0);
-    assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-    assert.equal(calculateNumber('DIVIDE', 0, 4.5), 0);
-    assert.equal(calculateNumber('DIVIDE', 0, -4.5), -0);
+    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+    assert.strictEqual(calculateNumber('SUM', -1.4, -4.5),-5);
+    assert.strictEqual(calculateNumber('SUM', 0, 0), 0);
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    assert.strictEqual(calculateNumber('SUBTRACT', -1.4, -4.5), 3);
+    assert.strictEqual(calculateNumber('SUBTRACT', 0, 0), 0);
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    assert.strictEqual(calculateNumber('DIVIDE', 0, 4.5), 0);
+    assert.strictEqual(calculateNumber('DIVIDE', 0, -4.5), -0);
     assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), NaN);
   });
 });
