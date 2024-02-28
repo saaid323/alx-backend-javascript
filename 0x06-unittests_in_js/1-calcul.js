0b939313e52a0bee71221d5e9d4f2825225010c0
saaid323 <saaid2538@gmail.com>
2024-02-28 15:36:44 +0000

    added 0-calcul.js, 0-calcul.test.js

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,13 +1,13 @@
 function calculateNumber (type, a, b) {
     let result;
     a = Math.round(a);
-    if (b === 0) return 'Zero';
     b = Math.round(b);
     if (type === 'SUM') {
       result = a + b;
     } else if (type === 'SUBTRACT') {
       result = a - b;
     } else if (type === 'DIVIDE') {
+      if (b === 0) return 'Zero';
       result = a / b;
     }
     return result;
