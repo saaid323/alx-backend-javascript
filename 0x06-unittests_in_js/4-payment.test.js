496bd69a808fc91f1ce0984b6a45dc72e8ab56a8
saaid323 <saaid2538@gmail.com>
2024-02-29 05:58:11 +0000

    added 4-payment.js, 4-payment.test.js

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,5 +1,5 @@
 const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
+const sendPaymentRequestToApi = require('./4-payment');
 const sinon = require('sinon');
 const assert = require('assert');
 
@@ -7,14 +7,13 @@ describe('sendPaymentRequestToApi stub suite', function() {
   it('spy and stub sendPaymentRequestToApi', function() {
     const stub = sinon.stub(Utils, 'calculateNumber');
     const spy = sinon.spy(console, 'log');
-    stub.returns(10);
-    sendPaymentRequestToApi(100, 20);
-    assert.strictEqual(stub.calledWith('SUM', 100, 20), true);
+    stub.withArgs('SUM', 100, 20).returns(10);
+    assert.strictEqual(stub('SUM', 100, 20), 10);
     assert.strictEqual(stub.callCount, 1);
+    sendPaymentRequestToApi(100, 20);
     assert.strictEqual(spy.calledWith('The total is: 10'), true);
     assert(spy.calledOnce);
     stub.restore();
     spy.restore();
-  })
-})
-
+  });
+});
