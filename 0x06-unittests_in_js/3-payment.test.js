5f2837cb4002d7429d4ea7b9efebace555ce5962
saaid323 <saaid2538@gmail.com>
2024-03-01 16:37:24 +0000

    added utils.js, 3-payment.js, 3-payment.test.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,7 +4,7 @@ const sinon = require('sinon');
 
 describe('sendPaymentRequestToApi suite', function() {
   it('spy sendPaymentRequestToApi', function() {
-    let spy = sinon.spy(console, 'log');
+    let spy = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
     sinon.assert.calledWith(spy, 'SUM', 100, 20);
     spy.restore();
